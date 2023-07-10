b131b0862d48a697eb32017158d03cee9e42e223
mini9707 <tqa0722@naver.com>
2023-07-10 15:41:32 +0900

    회원기능 구현

--- Week4/myselectshop/src/main/java/com/sparta/myselectshop/scheduler/Scheduler.java
+++ Week4/myselectshop/src/main/java/com/sparta/myselectshop/scheduler/Scheduler.java
@@ -23,7 +23,7 @@ public class Scheduler {
     private final ProductRepository productRepository;
 
     // 초, 분, 시, 일, 월, 주 순서
-    @Scheduled(cron = "0 0 1 * * *") // 매일 새벽 1시
+    @Scheduled(cron = "0 0 1 * * *") // @Scheduled : 지정한 시간마다 동작(cron "초, 분, 시, 일, 월, 주") -> 매일 새벽 1시
     public void updatePrice() throws InterruptedException {
         log.info("가격 업데이트 실행");
         List<Product> productList = productRepository.findAll();
--- Week4/myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ Week4/myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,7 +29,7 @@ public class ProductService {
     @Transactional  //업데이트 할 때 변경 감지 !!!
     public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
-        if(myprice < MIN_MY_PRICE) {
+        if (myprice < MIN_MY_PRICE) {
             throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
         }
 
@@ -36,13 +37,13 @@ public class ProductService {
                 new NullPointerException("해당 상품을 찾을 수 없습니다.")
         );
 
-        product.update(requestDto);
+        product.update(requestDto); // myprice 를 전달해 줘도 됨
 
         return new ProductResponseDto(product);
     }
 
     public List<ProductResponseDto> getProducts() {
-        List<Product> productList = productRepository.findAll();
+        List<Product> productList = productRepository.findAll(); // findAll() -> List 형태로 반환 !
         List<ProductResponseDto> responseDtoList = new ArrayList<>();
 
         for (Product product : productList) {
@@ -51,4 +52,33 @@ public class ProductService {
 
         return responseDtoList;
     }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new NullPointerException("해당 상품은 존재하지 않습니다.")
+        );
+        product.updateByItemDto(itemDto);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
